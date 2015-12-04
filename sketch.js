08d182be3ae6eb5a76464580113bfc870d2b7387
nat1508 <florn640@newschool.edu>
2015-12-04 18:55:33 -0500

    background

--- sketch.js
+++ sketch.js
@@ -1,16 +1,20 @@
+//variables
+var foodcourtImg;
+
+// images
+function preload() {
+	foodcourtImg = loadImage("images/foodcourt.png");
+
+}
+
 function setup() {
 	// create a place to draw
 	createCanvas(1270, 630);
 }
 
 function draw() {
-	// clear the background
-	background(0, 0, 0);
-
-	// set a fill color
-	fill(255, 255, 100);
+	image(foodcourtImg, 0, 0);
 
 
-	// draw a circle
-	ellipse(320, 180, 100, 100);
+
 }
